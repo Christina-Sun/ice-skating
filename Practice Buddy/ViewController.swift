fda6c9e5e03551abe8f6a0b6c713af91050a8d78
Christina-Sun <christinasun@students.berkeley.net>
2017-02-06 11:50:20 -0800

    1

---
+++ Practice Buddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice Buddy
+//
+//  Created by Period 3 on 2/2/17.
+//  Copyright © 2017 Sun Period 3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
