ce4f08ca73f7a73b9581db6e22febeebaa97eec1
Christina-Sun <christinasun@students.berkeley.net>
2017-02-10 11:13:07 -0800

    changed to slider, but when connecting to .swift illegal configuration error

--- Practice Buddy/ViewController.swift
+++ Practice Buddy/ViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Home screen elements:
     @IBOutlet weak var welcomeLabel: UILabel!
     
     @IBAction func startNewPractice(_ sender: AnyObject) {
     }
     
+    
+    //New practice screen elements:
     @IBAction func start(_ sender: AnyObject) {
     }
     
+//Illegal configuration error?
+//    @IBOutlet weak var move1: UITextField!
+//    
+//    @IBOutlet weak var slider1: UISlider!
     
+    
+    //Original swift stuff - untouched
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
